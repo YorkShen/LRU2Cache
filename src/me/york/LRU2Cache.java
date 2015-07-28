03b4e57887f74697768c3d1b9837b1b72c61bcf9
YorkShen <shenyuancs@gmail.com>
2015-07-28 23:05:19 +0800

    1. compareTo return V.compareTo 2. evict from the hashmap

--- src/me/york/LRU2Cache.java
+++ src/me/york/LRU2Cache.java
@@ -3,36 +3,35 @@ package me.york;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by sy on 15/7/24.
- */
-public class LRU2Cache<K,V extends Comparable<V>> {
-    private MaxHeap<V> maxHeap;
-    private Map<K,Node<V>> map;
+public class LRU2Cache<K, V extends Comparable<V>> {
+    private MaxHeap<K, V> maxHeap;
+    private Map<K, Node<K, V>> map;
 
-    public LRU2Cache(int cacheSize){
-        maxHeap=new MaxHeap<V>(cacheSize);
-        map=new HashMap<K, Node<V>>((int) ((float) cacheSize / 0.75F + 1.0F));
+    public LRU2Cache(int cacheSize) {
+        maxHeap = new MaxHeap<K, V>(cacheSize);
+        map = new HashMap<K, Node<K, V>>((int) ((float) cacheSize / 0.75F + 1.0F));
     }
 
-    public void put(K key,V value){
-        if(key!=null&&value!=null) {
-            Node<V> previous;
+    public void put(K key, V value) {
+        if (key != null && value != null) {
+            Node<K, V> previous;
             if ((previous = map.get(key)) != null) {
                 maxHeap.remove(previous.getIndex());
             }
-            previous=new Node<V>(value);
-            map.put(key,previous);
+            if (maxHeap.isFull())
+                map.remove(maxHeap.getMax().getKey());
+            previous = new Node<K, V>(key, value);
+            map.put(key, previous);
             maxHeap.add(previous);
         }
     }
 
-    public V get(K key){
-        V value=null;
-        if(key!=null){
-            Node<V> node=map.get(key);
-            if(node!=null){
-                value=node.getValue();
+    public V get(K key) {
+        V value = null;
+        if (key != null) {
+            Node<K, V> node = map.get(key);
+            if (node != null) {
+                value = node.getValue();
                 maxHeap.reVisited(node.getIndex());
             }
         }
